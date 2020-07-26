f725d69d4882cf8ce5a64a13c2b036cef12f0816
EC2 Default User <ec2-user@ip-172-31-12-216.ap-southeast-2.compute.internal>
2020-07-26 07:22:30 +0000

    upload to S3 now working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import React, {useState, useEffect} from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import Amplify, { Auth } from 'aws-amplify';
+import Storage from '@aws-amplify/storage'
+
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports);
+//Storage.configure({ level: 'private' });
 
 function App() {
+  
+  const [uploadPct, setUploadPct] = useState(0);
+  
+  console.log('Auth user', Auth.user);
+  
+  function calculatePct(fileSize, Upload){
+     const total = (Upload/fileSize)*100;
+     setUploadPct(Number((total).toFixed(0)));
+  }
+  
+  function onChange(e) {
+      const file = e.target.files[0];
+      console.log('file',  e.target.files);
+      console.log('file meta', {
+          size: file.size,
+          level: 'private',
+          contentType: file.type,
+          metadata: { uploader: 's3-file-manager', username: Auth.user.attributes.email}
+      });
+      
+       Storage.vault.put(
+          file.name, 
+          file, {
+          level: 'private',
+          contentType: file.type,
+          metadata: { uploader: 's3-file-manager', 
+                      username: Auth.user.attributes.email
+                    },
+          progressCallback(progress) {
+          calculatePct(progress.total, progress.loaded);  
+          console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+        },
+      })
+      .then (result => 
+          console.log('success', result)
+        )
+      .catch(err => console.log('error', err));
+  }
+
   return (
     <div className="App">
+    
       <header className="App-header">
         <p>
           S3 file manager
         </p>
-
       </header>
+      <input
+              type="file" 
+              onChange={(evt) => onChange(evt)}
+          />
+          
+          {uploadPct}%
+          <AmplifySignOut /> 
     </div>
   );
 }
-
-export default withAuthenticator(App)
+export default withAuthenticator(App);
