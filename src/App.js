18af61fd3f5c4741bb639cacfb85caf445634aff
Mav Peri <perima@amazon.com>
2020-07-26 07:44:16 +0000

    add basic layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, {useState, useEffect} from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify, { Auth } from 'aws-amplify';
-import Storage from '@aws-amplify/storage'
 
-import aws_exports from './aws-exports'
+//material UI
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+// amplify
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import Amplify, { Auth } from 'aws-amplify';
+import Storage from '@aws-amplify/storage';
+import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
-//Storage.configure({ level: 'private' });
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+
 
 function App() {
-  
+  const classes = useStyles();
   const [uploadPct, setUploadPct] = useState(0);
+  const [uploadPctTxt, setUploadPctTxt] = useState('');
   
   console.log('Auth user', Auth.user);
   
   function calculatePct(fileSize, Upload){
      const total = (Upload/fileSize)*100;
      setUploadPct(Number((total).toFixed(0)));
+     setUploadPctTxt(Number((total).toFixed(0)) + '%');
   }
   
   function onChange(e) {
@@ -50,18 +71,38 @@ function App() {
   return (
     <div className="App">
     
+    
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+           <Grid container justify="center" spacing={2}>
+                <Grid item key={1} justify="center" xs={2}>
+                   <Paper className={classes.paper}>
+                     S3 file manager
+                    </Paper>
+                    <Paper className={classes.paper} >
+                     <AmplifySignOut /> 
+                     </Paper>
+                </Grid>
+                
+                <Grid item item key={2} justify="center" xs={10} >
+                 <Paper className={classes.paper} >
+                       Upload file: 
+                        <input
+                            type="file" 
+                            onChange={(evt) => onChange(evt)}
+                        />
+                        {uploadPctTxt}
+                    </Paper>
+                </Grid>
+          </Grid>
+        </Grid>
+    </Grid>
+    
       <header className="App-header">
         <p>
-          S3 file manager
+         
         </p>
       </header>
-      <input
-              type="file" 
-              onChange={(evt) => onChange(evt)}
-          />
-          
-          {uploadPct}%
-          <AmplifySignOut /> 
     </div>
   );
 }
